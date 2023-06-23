0fbc2f05ae4791b06d33e502f2ff86b558067a42
Matvey Makeev <Armatingamedev@gmail.com>
2023-06-23 20:42:57 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+
+        * Привет cегодня тебя научу работать с github в Intellij idea
+
+        * Создание репозитория
+        * 1. Для начала создаём новый проект
+        * 2. Потом создаём файл в корневой папке проекта под названием '.gitignore'
+        * 3. Переходим по ссылке 'https://github.com/github/gitignore/blob/main/Global/JetBrains.gitignore' и копируем всё из этого файла
+        * 4. Вставляем файл
+        * 5. Теперь надо создать новый репозиторий
+        * 6. Переходим в 'VCS -> Share Project on GitHub'
+        * 7. Настраеваем 'Repository name' и 'Description' потом нажимаем на 'Add account -> Log in via GitHub' и дабавляем github аккаунк
+        * 8. И нажимаем 'Share'
+        * 9. Потом нажимаем на 'Add' и ждем
+        * 10. В правом нижнем углу нажимаем на ссылку и переходим в ваш репозиторий
+
+        * Создание ветки
+        -       
+
+        */
+    }
+}
